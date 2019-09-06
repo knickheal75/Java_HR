eb86f337870bb126aa26ea0ef7af6bb009c601ff
knickheal75 <blitzkrieg0705@gmail.com>
2019-09-07 01:24:51 +0530

    string problrm odd even scanner correct

--- day6string.java
+++ day6string.java
@@ -4,26 +4,20 @@ import java.util.*;
 public class Solution {
 
     public static void main(String[] args) {
-        Scanner scanner=new Scanner(System.in);
-        int T=scanner.nextInt();
-        
-        for (int i=1;i<=T+1;i++){
-            String s=scanner.nextLine();
-            String s1="",s2="";
-
-            char[] myCharArray=s.toCharArray();
-            
-            for (int j=0;j<s.length();j++){
-                if (j%2==0){
-                    s1 += myCharArray[j];
-                }
-                else{
-                    s2 += myCharArray[j];
-                }
+        Scanner scan = new Scanner(System.in);
+        int cases = scan.nextInt();
+        for(int c=0;c<cases;c++) {
+            String even, odd;
+            even = odd = "";
+            String myString = scan.next();
+            char[] charArray = myString.toCharArray();
+            for (int i = 0; i < charArray.length; i += 2) {
+                even += charArray[i];
             }
-            System.out.println(s1+" "+s2);
-
-            
+            for (int j = 1; j < charArray.length; j += 2) {
+                odd += charArray[j];
+            }
+            System.out.println(even + " " + odd);
         }
     }
 }
