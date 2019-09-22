772c362c737210415b0791a88b617d756299ffbb
knickheal75 <blitzkrieg0705@gmail.com>
2019-09-23 00:00:00 +0530

    bst depth

---
+++ day22bst.java
@@ -0,0 +1,56 @@
+import java.util.*;
+import java.io.*;
+class Node{
+    Node left,right;
+    int data;
+    Node(int data){
+        this.data=data;
+        left=right=null;
+    }
+}
+class Solution{
+
+	public static int getHeight(Node root){
+        int hleft=0;
+        int hright=0;
+
+        if(root.left!=null){
+            hleft=getHeight(root.left)+1;
+        }
+        if(root.right!=null){
+            hright=getHeight(root.right)+1;
+        }
+
+        return (hleft > hright ? hleft : hright);
+
+    }
+
+    public static Node insert(Node root,int data){
+        if(root==null){
+            return new Node(data);
+        }
+        else{
+            Node cur;
+            if(data<=root.data){
+                cur=insert(root.left,data);
+                root.left=cur;
+            }
+            else{
+                cur=insert(root.right,data);
+                root.right=cur;
+            }
+            return root;
+        }
+    }
+	 public static void main(String args[]){
+        Scanner sc=new Scanner(System.in);
+        int T=sc.nextInt();
+        Node root=null;
+        while(T-->0){
+            int data=sc.nextInt();
+            root=insert(root,data);
+        }
+        int height=getHeight(root);
+        System.out.println(height);
+    }
+}
